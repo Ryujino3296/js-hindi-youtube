299f95619bb2b15b4064136e395d076ae12ad8e7
Saurav Singh <saurav.singh.min21@itbhu.ac.in>
2024-06-30 17:15:31 +0000

    LOOPS

--- .devcontainer/04_CONTROLFLOW/maps.js
+++ .devcontainer/04_CONTROLFLOW/maps.js
@@ -39,13 +39,45 @@ for (const [key, value] of map) {
 USA :- United States of America
 Fr :- France */
 
-const myObject = {
+/*const myObject = {
     game1: 'NFS',
     game2: 'Spiderman'
-}
+}*/
 
 //OBJECTS ARE NOT ITRETABLE so we can't iterate over it 
 // for (const [key, value] of myObject) {
 //     console.log(key, ':-', value);
     
 // }
+
+
+
+// FOR IN LOOP 
+
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    //console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
