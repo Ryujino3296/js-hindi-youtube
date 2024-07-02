95d402dcf874575a5f1ba15073e0b0ba3c4b6a1e
Saurav Singh <saurav.singh.min21@itbhu.ac.in>
2024-07-02 10:48:39 +0000

    promise start

---
+++ .devcontainer/07_Advance/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async task 2");
+        resolve()
+    }, 1000)
+
+}).then(function(){
+    console.log("Async 2 resolved");
+})
