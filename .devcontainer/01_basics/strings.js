1d65d8f64e7af2466adc2dbcb7d59affdce45e5b
Saurav Singh <saurav.singh.min21@itbhu.ac.in>
2024-07-03 07:06:27 +0000

    strings

--- .devcontainer/01_basics/strings.js
+++ .devcontainer/01_basics/strings.js
@@ -5,19 +5,19 @@ const repoCount = 50
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hitesh-hc-com')
+const gameName = new String('hitesh-hc-com') // String declare krne ka ek aur tarika hai bs ye 
 
 // console.log(gameName[0]);
-// console.log(gameName.__proto__);
+// console.log(gameName.__proto__); // ye dikhata hai ki strings objects hai asal me 
 
 
 // console.log(gameName.length);
-//console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
 
 const newString = gameName.substring(0, 4)
-console.log(newString);
+// console.log(newString);
 
 const anotherString = gameName.slice(-8, 4)
 console.log(anotherString);
